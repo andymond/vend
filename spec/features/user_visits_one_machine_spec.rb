39f09ac731b95f53bb946328b14aca4c1e949840
Andrew Dymond <andy.dymond@gmail.com>
2018-02-16 09:18:04 -0700

    Add snacks relation, snack validation spec passing

--- spec/features/user_visits_one_machine_spec.rb
+++ spec/features/user_visits_one_machine_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 feature "user visits show page for one vending maching" do
   scenario "they see a list of the vending machines snacks & their prices" do
-    owner = Owner.new(name: "max")
+    owner = Owner.create(name: "max")
     machine = owner.machines.create(location: "basement")
     snack_1 = machine.snacks.create(name: "kitkat", price: 100)
     snack_2 = machine.snacks.create(name: "twix", price: 75)
