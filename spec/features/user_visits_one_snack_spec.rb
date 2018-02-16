f7f7d67ea69690ab5fc7b2c411d1356258382cbb
Andrew Dymond <andy.dymond@gmail.com>
2018-02-16 09:45:21 -0700

    user_sees_one_snack_spec passing

--- spec/features/user_visits_one_snack_spec.rb
+++ spec/features/user_visits_one_snack_spec.rb
@@ -1,12 +1,12 @@
-# require "rails_helper"
-#
-# describe "user visits snack show page" do
-#   xit "displays snack name & price" do
-#     snack = Snack.new(name: "cheetos", price: 150)
-#
-#     visit snack_path(snack)
-#
-#     expect(page).to have_content(snack.name)
-#     expect(page).to have_content(snack.price)
-#   end
-# end
+require "rails_helper"
+
+describe "user visits snack show page" do
+  it "displays snack name & price" do
+    snack = Snack.create(name: "cheetos", price: 150)
+
+    visit snack_path(snack)
+
+    expect(page).to have_content(snack.name)
+    expect(page).to have_content(snack.price)
+  end
+end
