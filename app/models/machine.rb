1dacdd0601e85d1567d0c26256b771818c7c48e3
Andrew Dymond <andy.dymond@gmail.com>
2018-02-16 09:35:53 -0700

    Add avg_price instance method to Machine

--- app/models/machine.rb
+++ app/models/machine.rb
@@ -2,4 +2,11 @@ class Machine < ApplicationRecord
   belongs_to :owner
   has_many :machine_snacks
   has_many :snacks, through: :machine_snacks
+
+  def avg_price
+    snack_list = snacks.map do |snack|
+      snack.price
+    end
+    snack_list.sum/snack_list.count
+  end
 end
