6a456851cc735b09889e832e7fefc8da6c05e807
Andrew Dymond <andy.dymond@gmail.com>
2018-02-16 09:10:22 -0700

    Add user visits one vending machine spec

---
+++ spec/features/user_visits_one_machine_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+feature "user visits show page for one vending maching" do
+  scenario "they see a list of the vending machines snacks & their prices" do
+    machine = Machine.new(location: "basement")
+    snack_1 = machine.snacks.create(name: "kitkat", price: "100")
+    snack_2 = machine.snacks.create(name: "twix", price: "75")
+
+    visit machine_path(machine)
+
+    expect(page).to have_content(snack_1.name)
+    expect(page).to have_content(snack_1.price)
+    expect(page).to have_content(snack_2.name)
+    expect(page).to have_content(snack_2.price)
+  end
+end
