0deedb3cb6e156d9684d2d6dc01e4394d6385a64
Andrew Dymond <andy.dymond@gmail.com>
2018-02-16 09:25:36 -0700

    Add it has many machine_snacks to snack_spec

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -8,4 +8,17 @@ describe Snack do
       expect(snack).to be_valid
     end
   end
+  context "Relationships" do
+    it "has many machine_snacks" do
+      owner = Owner.create(name: "max")
+      machine_1 = owner.machines.create(location: "basement")
+      machine_2 = owner.machines.create(location: "2nd floor")
+      snack = Snack.create(name: "snickers", price: 80)
+      machine_snack_1 = snack.machine_snacks.create(machine_id: machine_1.id)
+      machine_snack_2 = snack.machine_snacks.create(machine_id: machine_2.id)
+
+      expect(machine_snack_1).to be_valid
+      expect(machine_snack_2).to be_valid
+    end
+  end
 end
